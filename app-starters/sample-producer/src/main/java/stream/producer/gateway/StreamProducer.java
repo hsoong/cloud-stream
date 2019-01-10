d9f5c7245d6ddf307a450e8a29d50ef8e1a01927
Macmini <isongme@outlook.com>
2019-01-10 22:08:47 +0800

    add sample stream producer

---
+++ app-starters/sample-producer/src/main/java/stream/producer/gateway/StreamProducer.java
@@ -0,0 +1,55 @@
+package stream.producer.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.http.ResponseEntity;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import stream.producer.ProducerChannels;
+
+@SpringBootApplication
+@EnableBinding(ProducerChannels.class)
+@IntegrationComponentScan
+public class StreamProducer {
+
+    public static void main(String args[]) {
+        SpringApplication.run(StreamProducer.class, args);
+    }
+
+    @MessagingGateway
+    interface GreetingGateway {
+
+        @Gateway(requestChannel = ProducerChannels.BROADCAST)
+        void broadcastGreet(String msg);
+
+        @Gateway(requestChannel = ProducerChannels.DIRECT)
+        void directGreet(String msg);
+
+    }
+
+    @RestController
+    class GreetingProducer {
+        private final GreetingGateway gateway;
+
+        @Autowired
+        GreetingProducer(GreetingGateway gateway) {
+            this.gateway = gateway;
+        }
+
+        @RequestMapping(method = RequestMethod.GET, value = "/hi/{name}")
+        ResponseEntity<?> hi(@PathVariable String name) {
+            String message = "Hello, " + name + " !";
+            this.gateway.directGreet("Direct: " + message);
+            this.gateway.broadcastGreet("Broadcast: " + message);
+            return ResponseEntity.ok(message);
+        }
+    }
+
+}
---
+++ app-starters/src/main/java/xin/aircloud/stream/AppStartersApplication.java
@@ -0,0 +1,12 @@
+package xin.aircloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStartersApplication.class, args);
+	}
+}
